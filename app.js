a0197018e715de9521a4364124388f3a56f105bd
Jordan Rivers <j.russell.rivers@gmail.com>
2020-12-18 13:06:03 -0500

    anotha one

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const sickestFunc = () =>{
-    console.log('Veronica Rules!')
-}
+const sickestFunc = () => console.log('Veronica Rules!')
+const decentFunc = () => console.log('Micah is kinda cool.')
 
 sickestFunc()
+decentFunc()
